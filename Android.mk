703c54df00cbb8e45da31106de3843524cf9aedb
lolipuru <banstellazz@gmail.com>
2023-05-20 03:43:21 +0700

    extra: import apps from 64.1.A.0.891
    sony music : https://forum.xda-developers.com/t/4-2-sony-music-player-enhanced-for-all-devices.44091271
    
    except camera, due to 70mm parameter is changes
    
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter pdx206 pdx203 pdx204,$(TARGET_DEVICE)),)
+ifneq ($(filter pdx203 pdx204 pdx206,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
--- extra.mk
+++ extra.mk
@@ -3,26 +3,26 @@ PRODUCT_SOONG_NAMESPACES += \
 
 PRODUCT_COPY_FILES += \
 	vendor/sony/extra/overlay/system/etc/permissions/com.sony.device.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.sony.device.xml \
-    vendor/sony/extra/overlay/system/etc/permissions/com.sonymobile.home.resourceprovider.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.sonymobile.home.resourceprovider.xml \
+    vendor/sony/extra/overlay/product/etc/permissions/com.sonymobile.home.resourceprovider.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.sonymobile.home.resourceprovider.xml \
     vendor/sony/extra/overlay/system/etc/permissions/com.sonymobile.mmwdiapi.somc-mmwdiapi.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.sonymobile.mmwdiapi.somc-mmwdiapi.xml \
     vendor/sony/extra/overlay/system/etc/permissions/privapp-permissions-sony.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-sony.xml \
     vendor/sony/extra/overlay/product/etc/permissions/com.sonyericsson.idd.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.sonyericsson.idd.xml \
     vendor/sony/extra/overlay/product/etc/permissions/com.google.protobuf-2.3.0.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.protobuf-2.3.0.xml \
-    vendor/sony/extra/overlay/product/etc/permissions/privapp-permissions-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-product.xml 
+    vendor/sony/extra/overlay/product/etc/permissions/privapp-permissions-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-product.xml \
+    vendor/sony/extra/overlay/system_ext/etc/permissions/privapp-permissions-system-ext.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-system-ext.xml 
 
 PRODUCT_PACKAGES += \
     com.sony.device \
     com.sonymobile.home.resourceprovider \
     com.sonyericsson.idd_impl \
-    com.google.protobuf-2.3.0 \
+    com.google.protobuf-2.3.0-dummy \
     somc-ext-mmwdiapi \
     SonyMusic \
     CameraAddonPermission \
     CameraCommonPermission \
-    PortraitSelfie \
     ArtFilterCamera \
     CameraPanorama \
     SomcMovieCreator \
-    SomcColorGamut \
     SomcCinemaPro \
+    SomcVideoPro \
     SomcPhotoPro
